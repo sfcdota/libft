dd88f50dc1fac47a86927fafdb5829aef5b9afe6
sfcdota <sfcdota@gmail.com>
2020-05-27 14:30:33 +0300

    das

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cbach <cbach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 19:07:26 by cbach             #+#    #+#             */
-/*   Updated: 2020/05/27 14:17:48 by cbach            ###   ########.fr       */
+/*   Updated: 2020/05/27 14:28:45 by cbach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ char				*ft_strnstr(const char *big, const char *little, size_t len)
 	l = ft_strlen(little);
 	if (!l)
 		return ((char *)big);
-	while (*begin && len)
+	while (*begin && len && l <= len--)
 	{
-		while (l <= len--)
-		{
-			if (!ft_memcmp(begin, little, l))
-				return ((char *)begin);
-			begin++;
-		}
+		if (!ft_memcmp(begin, little, l))
+			return ((char *)begin);
+		begin++;
 	}
 	return (NULL);
 }
