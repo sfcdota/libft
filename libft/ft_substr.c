6f8643635dc698b7bf634f6656e9e80f14e527d6
sfcdota <sfcdota@gmail.com>
2020-05-28 20:53:12 +0300

    ASDF

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cbach <cbach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 21:21:55 by cbach             #+#    #+#             */
-/*   Updated: 2020/05/28 20:52:15 by cbach            ###   ########.fr       */
+/*   Updated: 2020/05/28 20:53:10 by cbach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	l = ft_strlen(s);
 	if (start >= l)
-		return ("");
+		return (ft_strdup(""));
 	sub = malloc(l - start < len ? l - start + 1 : len + 1);
 	if (sub)
 		ft_strlcpy(sub, &s[start], len + 1);
