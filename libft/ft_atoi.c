a6bab5fe5918c5394e6d840c22263b9346e0eaae
sfcdota <sfcdota@gmail.com>
2020-05-30 21:14:11 +0300

    asdf

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,21 @@
 /*   By: cbach <cbach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 17:18:38 by cbach             #+#    #+#             */
-/*   Updated: 2020/05/30 20:47:52 by cbach            ###   ########.fr       */
+/*   Updated: 2020/05/30 21:12:31 by cbach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int is_over(long long int i, int multiplier, int current)
+{
+	if ((i == 922337203685477580 && current > 54 && multiplier == 1)
+	|| (i == 922337203685477580 && current > 55 && multiplier == -1)
+	|| i > 922337203685477580)
+		return (1);
+	else
+		return (0);
+}
 
 int		ft_atoi(const char *nptr)
 {
@@ -29,14 +39,8 @@ int		ft_atoi(const char *nptr)
 	}
 	while (ft_isdigit(*nptr))
 	{
-		if (i > 922337203685477580 && *nptr > 54)
-		{
-			if (multiplier == 1)
-				return (-1);
-			else
-				if (multiplier == -1 && *nptr > 55)
-					return (0);
-		}
+		if (is_over(i, multiplier, *nptr))
+			return (multiplier == 1 ? -1 : 0);
 		i = i * 10 + (*nptr++ - 48);
 	}
 	return (i * multiplier);
